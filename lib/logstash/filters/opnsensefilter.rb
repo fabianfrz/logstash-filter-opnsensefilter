adc31086507f9839e1c09b3598ad4893849911b0
Fabian Franz <fabian.franz@students.fh-hagenberg.at>
2017-08-10 19:54:09 +0200

    remove comments

--- lib/logstash/filters/opnsensefilter.rb
+++ lib/logstash/filters/opnsensefilter.rb
@@ -2,21 +2,8 @@
 require "logstash/filters/base"
 require "logstash/namespace"
 
-# This  filter will replace the contents of the default 
-# message field with whatever you specify in the configuration.
-#
-# It is only intended to be used as an .
 class LogStash::Filters::Opnsensefilter < LogStash::Filters::Base
 
-  # Setting the config_name here is required. This is how you
-  # configure this filter from your Logstash config.
-  #
-  # filter {
-  #    {
-  #     message => "My message..."
-  #   }
-  # }
-  #
   config_name "opnsensefilter"
 
   # Replace the message with this value.
@@ -90,7 +77,6 @@ class LogStash::Filters::Opnsensefilter < LogStash::Filters::Base
       end
     end
 
-    # filter_matched should go in the last line of our successful code
     filter_matched(event)
   end # def filter
 end # class LogStash::Filters::Opnsensefilter
